7da77a6bc553a9fbc7cd492bd9540d097751d789
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-20 11:08:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const core = require('@actions/core');
+const ghCore = require('@actions/core');
 const github = require('@actions/github');
+const ghExec = require('@actions/exec');
 
-try {
+async function run () {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const nameToGreet = ghCore.getInput('who-to-greet');
+  ghCore.info(`Hello ${nameToGreet}!`);
+
+
+  //Get the JSON webhook payload for the event that triggered the workflow
+  const context = JSON.stringify(github.context, undefined, 2);
+  ghCore.info(`The github context: ${context}`);
+
+  //OpenShift 登录
+  //定义回调函数
+  //成功的返回值
+  let stdout = "";
+  //错误的返回值
+  let stderr = "";
+  const finalExecOptions = {};
+  finalExecOptions.listeners = {
+    stdout: (chunk) => {
+      stdout += chunk.toString();
+    },
+    stderr: (chunk) => {
+      stderr += chunk.toString();
+    },
+  };
+
+  //得到输入参数
+//   const openshiftServerUrl = ghCore.getInput('openshift_server_url');
+//   const openshiftToken = '--token=' + ghCore.getInput('openshift_token');
+
+  //组合参数
+  const ocExecArgs = ['clone', '-b', 'master' 'https://github.com/hayden194/build-source-code'];
+
+  //执行登录
+  const exitCode = await ghExec.exec('git.exe', ocExecArgs, finalExecOptions);
+  if (exitCode !== 0) {
+    let error = `${path.basename(EXECUTABLE)} exited with code ${exitCode}`;
+    if (stderr) {
+      error += `\n${stderr}`;
+    }
+    throw new Error(error);
+  }
+
+  //打印登录成功消息
+  ghCore.info('openshift login success message:' + stdout);
+
+  //输出参数
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  ghCore.setOutput("time", time);
 }
+
+run()
+  .then(() => {
+    ghCore.info("Success.");
+  })
+  .catch(ghCore.setFailed);
