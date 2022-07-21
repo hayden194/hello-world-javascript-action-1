df18755a20006a8cfc60d0da13acf755a8121f6d
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-21 14:19:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const ghCore = require('@actions/core');
 const github = require('@actions/github');
 const ghExec = require('@actions/exec');
 
-async function run () {
-  // `who-to-greet` input defined in action metadata file
+  //`who-to-greet` input defined in action metadata file
   const nameToGreet = ghCore.getInput('who-to-greet');
   ghCore.info(`Hello ${nameToGreet}!`);
+  
+  //define artifactory registry url
+  const registry_url="artifactrepo-dev:10743"
+  //get artifactory image path
+  const registry_image_path="";
   
   //get artifactory registry user
   const registry_user=ghCore.getInput('REGISTRY_USER');
   //get artifactory registry password
   const registry_password=ghCore.getInput('REGISTRY_PASSWORD'); 
+  //get artifactory registy repos tag version
+  const registy_repos_tag_version=ghCore.getInput('REGISTRY_REPOS_TAG_VERSION'); 
+  
+
+async function run () {
+  
 
 
   //Get the JSON webhook payload for the event that triggered the workflow
