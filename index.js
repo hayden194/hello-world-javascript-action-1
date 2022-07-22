d50ffd7cd0b340ce823355e96c1abcd5e5efe2f6
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-22 11:13:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,50 +2,52 @@ const ghCore = require('@actions/core');
 const github = require('@actions/github');
 const ghExec = require('@actions/exec');
 
-  //`who-to-greet` input defined in action metadata file
-  const nameToGreet = ghCore.getInput('who-to-greet');
-  ghCore.info(`Hello ${nameToGreet}!`);
-  
-  //define artifactory registry url
-  const registry_url="artifactrepo-dev:10743"
-  //get artifactory image path
-  const registry_image_path="";
-  
-  //get artifactory registry user
-  const registry_user=ghCore.getInput('REGISTRY_USER');
-  //get artifactory registry password
-  const registry_password=ghCore.getInput('REGISTRY_PASSWORD'); 
-  //get artifactory registy repos tag version
-  const registy_repos_tag_version=ghCore.getInput('REGISTRY_REPOS_TAG_VERSION'); 
-  
+//`who-to-greet` input defined in action metadata file
+const nameToGreet = ghCore.getInput('who-to-greet');
+ghCore.info(`Hello ${nameToGreet}!`);
+
+
+
+//get artifactory registry user
+const registry_user=ghCore.getInput('REGISTRY_USER');
+//get artifactory registry password
+const registry_password=ghCore.getInput('REGISTRY_PASSWORD'); 
+
+//get artifactory registry url
+const registry_url=ghCore.getInput('REGISTRY_URL');
+
+//get artifactory registry repository
+const registry_repository=ghCore.getInput('REGISTRY_REPOSITORY');
+
+//get artifactory registry repos tag name
+const registry_repos_tag_name=ghCore.getInput('REGISTRY_REPOS_TAG_NAME');
+
+//get artifactory image path
+const registry_image_path=registry_url+"/"+registry_repository+"/"+registry_repos_tag_name;
+
+//get artifactory registy repos tag version
+const registy_repos_tag_version=ghCore.getInput('REGISTRY_REPOS_TAG_VERSION'); 
+
+//成功的返回值
+let stdout = "";
+//错误的返回值
+let stderr = "";
+//定义回调函数
+const finalExecOptions = {};
+finalExecOptions.listeners = {
+  stdout: (chunk) => {
+    stdout += chunk.toString();
+  },
+  stderr: (chunk) => {
+    stderr += chunk.toString();
+  },
+};
 
 async function run () {
   
-
-
   //Get the JSON webhook payload for the event that triggered the workflow
   const context = JSON.stringify(github.context, undefined, 2);
   ghCore.info(`The github context: ${context}`);
-
-  //OpenShift 登录
-  //定义回调函数
-  //成功的返回值
-  let stdout = "";
-  //错误的返回值
-  let stderr = "";
-  const finalExecOptions = {};
-  finalExecOptions.listeners = {
-    stdout: (chunk) => {
-      stdout += chunk.toString();
-    },
-    stderr: (chunk) => {
-      stderr += chunk.toString();
-    },
-  };
-
-  //得到输入参数
-//   const openshiftServerUrl = ghCore.getInput('openshift_server_url');
-//   const openshiftToken = '--token=' + ghCore.getInput('openshift_token');
 
   //组合参数
   const ocExecArgs = ['clone', '-b', 'master','https://github.com/hayden194/build-source-code'];
